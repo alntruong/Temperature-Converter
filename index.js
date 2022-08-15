85544058129485a9e3bcf01c27e382de7ceb99c9
Alan Truong <alntruong@ucdavis.edu>
2022-08-15 13:03:51 -0700

    linked convert button and created function

---
+++ index.js
@@ -0,0 +1,7 @@
+const generateButton = document.getElementById("convert-button");
+
+generateButton.addEventListener("click", convertFunction);
+
+function convertFunction() {
+ alert("The convert button was clicked!")
+};
