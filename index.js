dce73577c59c8410ac4936fc33a2d2bf63634a02
Alan Truong <alntruong@ucdavis.edu>
2022-08-15 16:42:47 -0700

    worked on obtaining value of selection

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var selectedValue = document.getElementById("units").value;
+document.getElementById("units").addEventListener("change", getSelectedValue);
+/** added an onChange event listener to selection */
+
+function getSelectedValue() {
+ var selectedValue = document.getElementById("units").value;
+ console.log(selectedValue);
+}
+//** created a function that gives the value of selection */
+
+getSelectedValue();
 
 const buttonPress = document.getElementById("convert-button");
 
 buttonPress.addEventListener("click", convertFunction);
 
-
 function convertFunction() {
- alert(selectedValue);
+ alert("Button was pressed!");
 };
 
 
