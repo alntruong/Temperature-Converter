db91a11dc151cfdeba5060f41a7f8d23da787abe
Alan Truong <alntruong@ucdavis.edu>
2022-08-16 22:49:59 -0700

    moved vars into functions bc of scope limitation

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-document.getElementById("units").addEventListener("change", getSelectedValue);
-/** added an onChange event listener to selection */
-
-function getSelectedValue() {
- var selectedValue = document.getElementById("units").value;
- console.log(selectedValue);
-}
-//** created a function that gives the value of selection */
+document.getElementById("units").addEventListener("change", convertFunction);
+/** added an onChange event listener to selection and linked it to convertFunction */
 
 document.getElementById
-("number-input").addEventListener("blur", getTemperatureValue);
-
-function getTemperatureValue() {
- var temperatureValue = document.getElementById("number-input").value;
- console.log(temperatureValue);
-}
+("number-input").addEventListener("blur", convertFunction);
 
 const buttonPress = document.getElementById("convert-button");
 
 buttonPress.addEventListener("click", convertFunction);
 
 function convertFunction() {
- getTemperatureValue();
- getSelectedValue();
+ var selectedValue = document.getElementById("units").value;
+ console.log(selectedValue);
 
+ var temperatureValue = document.getElementById("number-input").value;
+ console.log(temperatureValue);
 };
 
 
